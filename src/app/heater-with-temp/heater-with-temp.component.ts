aaca38a36f176a8b77a0b8d86713b8cf7f725db4
Sanzhar Kanapin <silentsanzhar@gmail.com>
2023-05-12 16:55:09 +0200

    heater svg

--- src/app/heater-with-temp/heater-with-temp.component.ts
+++ src/app/heater-with-temp/heater-with-temp.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'heater-with-temp',
   templateUrl: './heater-with-temp.component.html',
   styleUrls: ['./heater-with-temp.component.css']
 })
-export class HeaterWithTempComponent {
+export class HeaterWithTempComponent implements OnChanges {
+  @Input('min-temp') minTemp: number = 0;
+  @Input('max-temp') maxTemp: number = 0;
+  @Input('target-temp') targetTemp: number = 0;
 
+  heaterNeedleAngle = 0;
+  heaterDiameter = 200;//px
+  heaterNeedleWidth = 2;
+  heaterRadius = (this.heaterDiameter / 2);
+  heaterRadiusOffset = this.heaterNeedleWidth * 2;
+  // subtract the needleWidth or the circle will overflow the SVG wrapper 
+
+  lengthOfLineOutsideOfCircle = this.heaterRadius + this.heaterRadiusOffset - (this.heaterRadius / Math.sqrt(2));
+
+  ngOnChanges(): void {
+    this.heaterNeedleAngle = this.targetTemp / (this.maxTemp - this.minTemp) * 270;
+  }
 }
+
+
+  /*
+  diameterOfRect = (this.heaterRadius + this.heaterRadiusOffset) * Math.sqrt(2);
+  diameterOfSmallerRect = this.diameterOfRect - this.heaterRadius;
+  lengthOfLineOutsideOfCircle = this.heaterRadius + Math.sqrt(2) - (this.heaterRadius / Math.sqrt(2));
+  */
