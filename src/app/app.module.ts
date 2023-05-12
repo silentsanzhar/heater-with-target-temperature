7efe91e0c2eebf4a5767665d17f9fb5432a30c7d
Sanzhar Kanapin <silentsanzhar@gmail.com>
2023-05-12 12:07:58 +0200

    add new heater comp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaterWithTempComponent } from './heater-with-temp/heater-with-temp.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaterWithTempComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/heater-with-temp/heater-with-temp.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'heater-with-temp',
+  templateUrl: './heater-with-temp.component.html',
+  styleUrls: ['./heater-with-temp.component.css']
+})
+export class HeaterWithTempComponent {
+
+}
